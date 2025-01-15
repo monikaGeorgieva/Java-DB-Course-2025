13a4ca7691b0472a2b14c1154c1bda89c84b6334
Monika Georgieva <monikcho0o@abv.bg>
2025-01-15 21:42:36 +0000

    Update exercises_basic_CRUD.sql

--- MySQL/4.Exercise_Basic_CRUD/exercises_basic_CRUD.sql
+++ MySQL/4.Exercise_Basic_CRUD/exercises_basic_CRUD.sql
@@ -12,3 +12,49 @@ ORDER BY department_id;
 SELECT name FROM 
 departments;
 
+-- 3.Find salary of Each Employee
+SELECT `first_name`,`last_name`,`salary`
+ FROM employees
+ ORDER BY employee_id;
+ 
+ -- 4 Find full name of each employee
+ SELECT `first_name`,`middle_name`,`last_name`
+ FROM employees
+ ORDER BY employee_id;
+ 
+ -- 5 Find email Adress of Each Employee
+ 
+SELECT CONCAT(first_name, '.' ,last_name, '@softuni.bg') AS 'full_email_adress'
+from employees;
+
+-- 6.Find all the diffenrent employees salaries
+SELECT DISTINCT salary 
+FROM employees;
+
+-- 7. Find all Information abouth Employees
+
+SELECT
+employee_id AS id,
+first_name as `First Name`,
+last_name AS 'Last Name',
+middle_name AS 'Middle Name',
+job_title AS 'Job Title',
+department_id AS 'Dept ID',
+manager_id AS 'Mngr ID',
+hire_date AS 'Hire Date',
+salary,
+address_id 
+FROM employees
+WHERE job_title = 'Sales Representative'
+ORDER BY id;
+
+
+
+
+ 
+ 
+ 
+ 
+
+
+
