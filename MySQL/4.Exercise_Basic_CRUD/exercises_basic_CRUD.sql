9c5d92f19385124a0f2aee970e191f22b747155e
Monika Georgieva <monikcho0o@abv.bg>
2025-01-15 22:36:40 +0000

    Update exercises_basic_CRUD.sql

--- MySQL/4.Exercise_Basic_CRUD/exercises_basic_CRUD.sql
+++ MySQL/4.Exercise_Basic_CRUD/exercises_basic_CRUD.sql
@@ -48,11 +48,52 @@ FROM employees
 WHERE job_title = 'Sales Representative'
 ORDER BY id;
 
+-- 8.Find Names of all employees by salary range
+SELECT first_name,last_name,job_title
+FROM employees
+WHERE salary >= 20000 && salary <= 30000;
+
+-- 9.Find Names of ALL employees whose salary is 25000,140000 or 236000
+SELECT 
+concat_ws(' ',first_name,middle_name,last_name) AS 'Full Name'
+FROM employees
+WHERE salary IN (25000,1400,12500);
+
+-- 10.Find all Employees Without Manager
+SELECT first_name,last_name, salary FROM employees
+WHERE salary > 50000
+ORDER BY salary DESC;
+
+-- 11.Find all Employees without Manager
+SELECT first_name,last_name from employees
+WHERE manager_id is NULL;
+
+-- 12.Find best 5 paid employees
+SELECT first_name,last_name 
+from employees 
+ORDER BY salary DESC
+LIMIT 5;
+
+-- 13.Find all employees Except Marketing
+SELECT first_name,last_name
+FROM employees
+where job_title != 'Marketing';
+
+-- 14.Sort Employees Table
+SELECT
+employee_id AS id,
+first_name as `First Name`,
+last_name AS 'Last Name',
+middle_name AS 'Middle Name',
+job_title AS 'Job Title',
+department_id AS 'Dept ID',
+manager_id AS 'Mngr ID',
+hire_date AS 'Hire Date',
+salary, address_id FROM employees
+ORDER BY salary DESC,first_name,last_name DESC,middle_name; 
 
 
 
- 
- 
  
  
 
