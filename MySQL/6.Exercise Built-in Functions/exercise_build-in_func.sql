46b83152b89ea5535c1191606fa3de48cff59c95
Monika Georgieva <monikcho0o@abv.bg>
2025-01-30 20:05:59 +0000

    Update exercise_build-in_func.sql

--- MySQL/6.Exercise Built-in Functions/exercise_build-in_func.sql
+++ MySQL/6.Exercise Built-in Functions/exercise_build-in_func.sql
@@ -28,5 +28,27 @@ WHERE job_title NOT LIKE '%engineer%'
 ORDER BY employee_id;
 
 -- 05. Find Towns with Name Length
+SELECT name  
+FROM towns
+WHERE LENGTH(name) IN (5,6)
+ORDER BY name ASC;
+
+-- 6. Find Towns Starting With
+SELECT town_id,name
+FROM towns
+WHERE LEFT(name,1) IN ('M','K','B','E')
+ORDER BY name;
+
+-- 07. Find Towns Not Starting With
+SELECT town_id,name
+FROM towns
+WHERE LEFT(name,1) NOT IN ('R','B','D')
+ORDER BY name asc;
+
+-- 08. Create View Employees Hired After
+CREATE VIEW v_employees_hired_after_2000 AS
+SELECT first_name,last_name FROM employees
+WHERE YEAR(hire_date) > 2000;
 
 
+SELECT * from v_employees_hired_after_2000;
