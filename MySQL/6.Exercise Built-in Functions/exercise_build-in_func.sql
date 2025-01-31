b32d7c1b3ac6879d02195c0bfe42b6c9f2f72e90
Monika Georgieva <monikcho0o@abv.bg>
2025-01-31 15:29:47 +0000

    Update exercise_build-in_func.sql

--- MySQL/6.Exercise Built-in Functions/exercise_build-in_func.sql
+++ MySQL/6.Exercise Built-in Functions/exercise_build-in_func.sql
@@ -52,3 +52,44 @@ WHERE YEAR(hire_date) > 2000;
 
 
 SELECT * from v_employees_hired_after_2000;
+
+-- 9. Length of Last Name
+SELECT first_name,last_name
+FROM employees
+WHERE LENGTH(last_name) = 5;
+
+-- Part II – Queries for Geography Database
+
+-- 10. Countries Holding 'A' 3 or More Times
+SELECT country_name,iso_code
+FROM countries
+WHERE country_name LIKE '%A%A%A%'
+ORDER BY iso_code; 
+
+-- 11. Mix of Peak and River Names
+SELECT peak_name, river_name,
+LOWER(CONCAT(peak_name, substring(river_name, 2))) AS mix
+FROM peaks, rivers
+WHERE RIGHT(peak_name, 1) =  LEFT(river_name,1)
+ORDER BY mix;
+
+-- Part III – Queries for Diablo Database
+
+-- 12. Games from 2011 and 2012 Year
+SELECT name,DATE_FORMAT(start,'%Y-%m-%d') AS start FROM games
+WHERE EXTRACT(YEAR FROM start) IN (2011,2012)
+ORDER BY start, name
+LIMIT 50 ;
+
+-- 13. User Email Providers
+SELECT user_name, SUBSTRING(email, LOCATE('@', email) + 1) AS `email provider`
+FROM users
+ORDER BY `email provider`, user_name;
+
+-- 14. Get Users with IP Address Like Pattern
+
+-- 15. Show All Games with Duration and Part of the Day
+
+
+
+
